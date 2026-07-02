93896895492b3923f8e4b8e21337f25cebe81bfb
SergioVillanoReyes <villanoreyes@hotmail.com>
2026-07-02 11:19:44 -0600

    core(): eslint configuration rules has been implemented

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'jsx-quotes': ['error', 'prefer-double'],
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
